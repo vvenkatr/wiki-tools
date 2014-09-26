c8fc67eb5daee148fd51ef8fe67c763db2e2078d
Vasanth Venkatraman <vasanthchandramouli@gmail.com>
2014-09-26 11:08:26 +0200

    Added Logger
        * Logger Used

--- src/main/java/de/mpii/wikitools/WikiRevTools.java
+++ src/main/java/de/mpii/wikitools/WikiRevTools.java
@@ -12,8 +12,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
@@ -30,6 +30,8 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WikiRevTools {
 
@@ -38,17 +40,19 @@ public class WikiRevTools {
   private static final String PAGE_ID_TAG = "id";
 
   private static final String PAGE_TITLE_TAG = "title";
-  
+
   private static final String PAGE_REVISION_TAG = "revision";
 
   private static final String PAGE_REDIRECT_TAG = "redirect";
 
   private static final String PAGE_REVISION_TEXT_TAG = "text";
-  
+
   private static Pattern pattern = Pattern.compile("\\[\\[(.*?)\\]\\]");
-  
+
   private static Options commandLineOptions;
-  
+
+  private static Logger logger_ = LoggerFactory.getLogger(WikiRevTools.class);
+
   public static Map<String, String> map(File source, File target) throws IOException, XMLStreamException {
     return map(source, target, true);
   }
@@ -65,7 +69,7 @@ public class WikiRevTools {
     XMLInputFactory factory = XMLInputFactory.newInstance();
     XMLEventReader targetReader = factory.createXMLEventReader(new FileReader(target));
     XMLEventReader sourceReader = factory.createXMLEventReader(new FileReader(source));
-    
+
     // variables to store page related information
     int pageId = -1;
     String title = null;
@@ -76,27 +80,28 @@ public class WikiRevTools {
      *  - Ignore revision tag and all its children (including <id>, text)
      *  - Load Id,Title under Page tag
      */
+    logger_.info("Scanning Target for Page Id, Title info...");
     while (targetReader.hasNext()) {
       event = targetReader.nextEvent();
       if(event.isStartDocument() || event.isEndDocument()) {
         // do nothing
       } else if (event.isStartElement()) {
         StartElement startElement = event.asStartElement();
-        String strStartElement = startElement.getName().getLocalPart(); 
+        String strStartElement = startElement.getName().getLocalPart();
         if(strStartElement.equals(PAGE_REVISION_TAG)) {
           processingRevisionTag = true;
         }
 
         if(!processingRevisionTag) {
           if(strStartElement.equals(PAGE_ID_TAG)) {
-            pageId = Integer.parseInt(targetReader.nextEvent().asCharacters().getData());            
+            pageId = Integer.parseInt(targetReader.nextEvent().asCharacters().getData());
           } else if(strStartElement.equals(PAGE_TITLE_TAG)) {
             title = targetReader.nextEvent().asCharacters().getData();
           }
-        }                       
+        }
       } else if(event.isEndElement()) {
         EndElement endElement = event.asEndElement();
-        String strEndElement = endElement.getName().getLocalPart(); 
+        String strEndElement = endElement.getName().getLocalPart();
         if (strEndElement.equals(PAGE_TAG)) {
           // once we reach end of page, we can update the id and title retrieved for the page in map
           idTitleMap.put(pageId, title);
@@ -110,6 +115,7 @@ public class WikiRevTools {
       }
     }
 
+    logger_.info("Scanning Target Dump for REDIRECTS...");
     // iterate over target again and resolve REDIRECTS ( and DISAMBIGUATION )
     targetReader = factory.createXMLEventReader(new FileReader(target));
     boolean extractRedirectText = false;
@@ -120,11 +126,11 @@ public class WikiRevTools {
       event = targetReader.nextEvent();
       if (event.isStartElement()) {
         StartElement startElement = event.asStartElement();
-        String startElementName = startElement.getName().getLocalPart(); 
+        String startElementName = startElement.getName().getLocalPart();
 
         if(!processingRevisionTag) {
           if(startElementName.equals(PAGE_ID_TAG)) {
-            pageId = Integer.parseInt(targetReader.nextEvent().asCharacters().getData());            
+            pageId = Integer.parseInt(targetReader.nextEvent().asCharacters().getData());
           } else if(startElementName.equals(PAGE_REDIRECT_TAG)) {
             extractRedirectText = true;
           } else if(startElementName.equals(PAGE_REVISION_TAG)) {
@@ -133,19 +139,19 @@ public class WikiRevTools {
         } else {
           // within revision tag : look out for redirection
           if(extractRedirectText && startElementName.equals(PAGE_REVISION_TEXT_TAG)) {
-            redirectMatcher = pattern.matcher(targetReader.getElementText());            
+            redirectMatcher = pattern.matcher(targetReader.getElementText());
             while(redirectMatcher.find()) {
               redirectTitle = redirectMatcher.group(1);
             }
             extractRedirectText = false;
           }
-        }            
+        }
       }
 
       if(pageId != -1 && redirectTitle != null) {
         redirectId = titleIdMap.get(redirectTitle);
         redirectIds.put(pageId, redirectId);
-        redirectTitle = null;  
+        redirectTitle = null;
       }
 
       if (event.isEndElement()) {
@@ -155,11 +161,12 @@ public class WikiRevTools {
           pageId = -1;
           title = null;
         }else if(endElement.getName().getLocalPart().equals(PAGE_REVISION_TAG)) {
-          processingRevisionTag = false;          
+          processingRevisionTag = false;
         }
       }
     }
 
+    logger_.info("Scanning source dump...");
     // finally iterate over source dump file and construct the final change map
     Map<String, String> finalMap = new HashMap<String, String>();
     while(sourceReader.hasNext()) {
@@ -168,10 +175,10 @@ public class WikiRevTools {
         StartElement startElement = event.asStartElement();
 
         if(pageId == -1 && startElement.getName().getLocalPart().equals(PAGE_ID_TAG)) {
-            pageId = Integer.parseInt(sourceReader.nextEvent().asCharacters().getData());
+          pageId = Integer.parseInt(sourceReader.nextEvent().asCharacters().getData());
         } else if( title == null && startElement.getName().getLocalPart().equals(PAGE_TITLE_TAG)) {
-            title = sourceReader.nextEvent().asCharacters().getData();
-        }               
+          title = sourceReader.nextEvent().asCharacters().getData();
+        }
       }
 
       if(pageId != -1 && title != null) {
@@ -184,9 +191,9 @@ public class WikiRevTools {
         if(redirectId != pageId) {
           targetTitle = idTitleMap.get(redirectId);
         } else {
-          targetTitle = idTitleMap.get(pageId);  
+          targetTitle = idTitleMap.get(pageId);
         }
-        // check whether the source dump's id is pointing to same title        
+        // check whether the source dump's id is pointing to same title
         if(!title.equals(targetTitle) || includeUnchangedEntries) {
           finalMap.put(title, targetTitle);
         }
@@ -213,25 +220,25 @@ public class WikiRevTools {
 
   public static void mapToFile(File source, File target, File output, boolean includeUnchangedEntries) throws IOException, XMLStreamException {
     Map<String, String> result = map(source, target, includeUnchangedEntries);
-    System.out.println("Writing results to file : " + output.getName());
+    logger_.debug("Writing results to file : " + output.getName());
     try{
       // just in case delete any old file
       output.delete();
       writeFileContent(output, result);
       System.out.println("Output written");
     }catch(IOException ioe) {
-      System.out.println("Failed to write results to file");
+      logger_.info("Failed to write results to file");
     }
   }
-  
+
   @SuppressWarnings("static-access")
   private static Options buildCommandLineOptions() throws ParseException {
     Options options = new Options();
     options
-        .addOption(OptionBuilder
-            .withLongOpt("cmd")
-            .withDescription(
-                "Command to be executed (Currently supports only MAP command)")
+    .addOption(OptionBuilder
+        .withLongOpt("cmd")
+        .withDescription(
+            "Command to be executed (Currently supports only MAP command)")
             .isRequired()
             .hasArg()
             .create("c"));
@@ -240,58 +247,58 @@ public class WikiRevTools {
         .withLongOpt("source")
         .withDescription(
             "Source dump to be mapped")
-        .hasArg()
-        .withArgName("SOURCEDUMP")
-        .create("s"));
+            .hasArg()
+            .withArgName("SOURCEDUMP")
+            .create("s"));
     options
     .addOption(OptionBuilder
         .withLongOpt("target")
         .withDescription(
             "Target dump to be check")
-        .hasArg()
-        .withArgName("TARGETDUMP")
-        .create("t"));
+            .hasArg()
+            .withArgName("TARGETDUMP")
+            .create("t"));
     options
     .addOption(OptionBuilder
         .withLongOpt("output")
         .withDescription(
             "Write to file")
-        .hasArg()
-        .withArgName("FILENAME")
-        .create("w"));
+            .hasArg()
+            .withArgName("FILENAME")
+            .create("w"));
     options.addOption(OptionBuilder.withLongOpt("help").create('h'));
     return options;
   }
-  
+
   private static void printHelp(Options commandLineOptions) {
     String header = "\n\nWiki Revision Tool:\n\n";
     HelpFormatter formatter = new HelpFormatter();
-    formatter.printHelp("WikiRevTools", header, 
+    formatter.printHelp("WikiRevTools", header,
         commandLineOptions, "", true);
     System.exit(0);
   }
-  
+
   private static void writeFileContent(File file, Map<String, String> hshResults) throws IOException {
-      BufferedWriter writer = getBufferedWriter(file);
-      for(Entry<String, String> e : hshResults.entrySet()) {
-        writer.append(e.getKey() + "\t" + e.getValue()+"\n");
-      }
-      writer.flush();
-      writer.close();
+    BufferedWriter writer = getBufferedWriter(file);
+    for(Entry<String, String> e : hshResults.entrySet()) {
+      writer.append(e.getKey() + "\t" + e.getValue()+"\n");
+    }
+    writer.flush();
+    writer.close();
   }
-  
+
   private static BufferedWriter getBufferedWriter(File file) throws IOException {
-      // need to append entries to the file
-      return new BufferedWriter(new FileWriter(file, true));
+    // need to append entries to the file
+    return new BufferedWriter(new FileWriter(file, true));
   }
-  
+
   public static void main(String args[]) throws Exception {
     commandLineOptions = buildCommandLineOptions();
     CommandLineParser parser = new PosixParser();
     CommandLine cmd = null;
-    
+
     try {
-      cmd = parser.parse(commandLineOptions, args); 
+      cmd = parser.parse(commandLineOptions, args);
     } catch (MissingOptionException e) {
       System.out.println("\n\n" + e + "\n\n");
       printHelp(commandLineOptions);
@@ -299,7 +306,7 @@ public class WikiRevTools {
     if (cmd.hasOption("h")) {
       printHelp(commandLineOptions);
     }
-    
+
     String cmdStr = "";
     cmdStr = cmd.getOptionValue('c');
     switch (cmdStr) {
@@ -308,9 +315,9 @@ public class WikiRevTools {
           System.out.println("\n\n MAP command needs SOURCE and TARGET options.\n\n");
           printHelp(commandLineOptions);
         }
-        
+
         String srcDump = cmd.getOptionValue('s');
-        String targetDump = cmd.getOptionValue('t');       
+        String targetDump = cmd.getOptionValue('t');
         if(cmd.hasOption('w')) {
           String outputFile = cmd.getOptionValue('w');
           mapToFile(new File(srcDump), new File(targetDump), new File(outputFile));
